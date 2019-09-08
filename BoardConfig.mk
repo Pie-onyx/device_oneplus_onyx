26da824010a8213ca2708d42a0df5f593a75ef0f
YumeMichi <do4suki@gmail.com>
2019-09-09 00:14:22 +0800

    onyx: Add temporary sepolicies for Q bringup
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,8 +165,8 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 # include device/qcom/sepolicy-legacy/sepolicy.mk
 
-# BOARD_SEPOLICY_DIRS += \
-#     $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(PLATFORM_PATH)/sepolicy_tmp
 
 # SHIMS
 TARGET_LD_SHIM_LIBS := \
